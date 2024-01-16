0c2df90360c1b789f8057e5330d0c9e9d3d0dccf
Daniel Theuri <dantheuri17@gmail.com>
2024-01-16 07:01:11 +0300

    finished with desktop responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,38 @@
-import mobileHeaderImg from '../assets/images/mobile/image-hero.jpg'
+import mobileHeaderImg from "../assets/images/mobile/image-hero.jpg";
+import desktopHeaderImg from "../assets/images/desktop/image-hero.jpg";
+
+
+import logo from "../assets/images/logo.svg";
 
 const Header = () => {
 	return (
-		<header className="max-w-[23.4375rem] relative">
+		<header className="max-w-[23.4375rem] relative md:max-w-none md:w-full">
+			<img
+				src={logo}
+				alt=""
+				className="absolute hidden sm:block md:left-[10.31rem] md:top-[4rem]"
+			/>
 			<picture>
 				<source media="(max-width: 480px)" srcSet={mobileHeaderImg} />
-				<img src={mobileHeaderImg} alt="" className="h-[40.625rem]" />
+				<source media="(min-width: 1024px)" srcSet={desktopHeaderImg} />
+
+				<img
+					src={mobileHeaderImg}
+					alt=""
+					className="h-[40.625rem] md:w-full md:object-contain lg:object-fill"
+				/>
 			</picture>
-			<nav className="border border-black">
-				About Careers Events Products Support
+			<nav className="absolute  text-white md:top-[4.25rem] md:left-[55.94rem]">
+				<ul className="flex font-alata md:gap-x-[2rem] md:font-[400]">
+					<li>About</li>
+					<li>Careers</li>
+					<li>Events</li>
+					<li>Products</li>
+					<li>Support</li>
+				</ul>
 			</nav>
-			<div className="absolute top-[14.19rem] left-[1.5rem] px-[1.5rem] pt-[1.63rem] pb-[1.5rem] w-[20.4375rem] border  border-white ">
-				<h1 className="w-[17.4375rem] font-josefinSans uppercase text-white text-[2.5rem] font-[300] leading-[2.375rem]">
+			<div className="absolute top-[14.19rem] md:top-[14.13rem] left-[1.5rem] md:left-[10.31rem] md:p-[2.5rem] px-[1.5rem] pt-[1.63rem] pb-[1.5rem] w-[20.4375rem] md:w-[40.625rem] border  border-white ">
+				<h1 className="w-[17.4375rem] font-josefinSans uppercase text-white text-[2.5rem] md:text-[4.5rem] font-[300] leading-[2.375rem] md:leading-[4.375rem]">
 					Immersive experiences that deliver
 				</h1>
 			</div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,110 +1,136 @@
-import interactiveImage from "../assets/images/mobile/image-interactive.jpg";
+import mobileInteractiveImage from "../assets/images/mobile/image-interactive.jpg";
+import desktopInteractiveImage from '../assets/images/desktop/image-interactive.jpg';
 
 import mobileDeepEarth from "../assets/images/mobile/image-deep-earth.jpg";
+import desktopDeepEarth from '../assets/images/desktop/image-deep-earth.jpg';
+
 import mobileNightArcade from "../assets/images/mobile/image-night-arcade.jpg";
+import desktopNightArcade from '../assets/images/desktop/image-night-arcade.jpg';
+
 import mobileSoccerTeam from "../assets/images/mobile/image-soccer-team.jpg";
+import desktopSoccerTeam from '../assets/images/desktop/image-soccer-team.jpg';
+
 import mobileGridImage from "../assets/images/mobile/image-grid.jpg";
+import desktopGridImage from '../assets/images/desktop/image-grid.jpg';
+
 import mobileFromAbove from "../assets/images/mobile/image-from-above.jpg";
+import desktopFromAbove from '../assets/images/desktop/image-from-above.jpg';
+
 import mobilePocketBorealis from "../assets/images/mobile/image-pocket-borealis.jpg";
+import desktopPocketBorealis from '../assets/images/desktop/image-pocket-borealis.jpg';
+
 import mobileCuriosityImage from "../assets/images/mobile/image-curiosity.jpg";
+import desktopCuriosityImage from '../assets/images/desktop/image-curiosity.jpg';
+
 import mobileFisheye from "../assets/images/mobile/image-fisheye.jpg";
+import desktopFisheye from '../assets/images/desktop/image-fisheye.jpg';
 
 const Main = () => {
 	return (
-		<main className="max-w-[23.4375rem] mt-[6rem]">
-			<picture>
-				<source media="(max-width: 480px)" srcSet={interactiveImage} />
-				<img src="" alt="" />
-			</picture>
-			<div className="grid grid-cols-1 gap-y-[1rem] mt-[5rem] px-[3rem]">
-				<h1 className="uppercase font-josefinSans text-[2rem] text-center font-[300] leading-[2rem]">
-					The leader in interactive VR
-				</h1>
-				<p className="font-alata text-[0.9375rem] leading-[1.5625rem] font-[400] opacity-[0.5]">
-					Founded in 2011, Loopstudios has been producing world-class virtual
-					reality projects for some of the best companies around the globe. Our
-					award-winning creations have transformed businesses through digital
-					experiences that bind to their brand. See all Deep earth Night arcade
-					Soccer team VR The grid From up above VR Pocket borealis The curiosity
-					Make it fisheye
-				</p>
+		<main className="max-w-[23.4375rem] mt-[6rem] md:mt-[10rem] md:max-w-none">
+			<div className="pl-[10.25rem] relative">
+				<picture>
+					<source media="(max-width: 480px)" srcSet={mobileInteractiveImage} />
+					<source
+						media="(min-width: 1024px)"
+						srcSet={desktopInteractiveImage}
+					/>
+					<img src="" alt="" className="md:h-[31.25rem]" />
+				</picture>
+				<div className="absolute grid grid-cols-1 bg-white  gap-y-[1rem] md:gap-y-[1.56rem] md:top-[11.44rem] md:right-[10.31rem] mt-[5rem] md:mt-0 px-[3rem] md:pl-[6rem] md:pt-[6rem] md:w-[33.8125rem] md:h-[19.8125rem]">
+					<h1 className="uppercase font-josefinSans text-[2rem] md:text-[3rem] text-center md:text-left font-[300] leading-[2rem] md:leading-[3rem]">
+						The leader in interactive VR
+					</h1>
+					<p className="font-alata text-[0.9375rem] leading-[1.5625rem] md:w-[27.8125rem] font-[400] opacity-[0.5]">
+						Founded in 2011, Loopstudios has been producing world-class virtual
+						reality projects for some of the best companies around the globe.
+						Our award-winning creations have transformed businesses through
+						digital experiences that bind to their brand.
+					</p>
+				</div>
 			</div>
-			<section className="px-[1.5rem]">
-				<h2 className="text-[2rem] font-[300] uppercase font-josefinSans text-center mt-[6rem] ">
+
+			<section className="px-[1.5rem] md:px-[10.25rem]">
+				<h2 className="text-[2rem] md:text-[3rem] font-[300] uppercase font-josefinSans text-center md:text-left mt-[6rem]">
 					Our creations
 				</h2>
-				<div className="grid grid-cols-1 gap-y-[1.5rem] mt-[3rem] font-josefinSans text-[1.5rem] font-[300] leading-[1.5rem]">
+				<div className="grid grid-cols-1 md:grid-cols-[repeat(4,16rem)] uppercase md:gap-x-[1.88rem]  md:grid-rows-2  gap-y-[1.5rem] mt-[3rem] font-josefinSans text-[1.5rem] md:text-[2rem] font-[300] leading-[1.5rem] md:leading-[2rem]">
 					<div className="relative">
-						<h3 className=" text-white absolute left-[1.25rem] top-[3.25rem] w-[5rem] h-[3rem] ">
+						<h3 className=" text-white absolute left-[1.25rem] md:left-[2.5rem] md:top-[22.12rem] top-[3.25rem] w-[5rem] h-[3rem] ">
 							Deep Earth
 						</h3>
 						<picture>
 							<source media="(max-width: 480px)" srcSet={mobileDeepEarth} />
+							<source media="(min-width: 1024px)" srcSet={desktopDeepEarth} />
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
 
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] bottom-[1.25rem] text-white w-[6.1875rem]">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] text-white w-[6.1875rem]">
 							Night Arcade
 						</h3>
 						<picture>
 							<source media="(max-width: 480px)" srcSet={mobileNightArcade} />
+							<source media="(min-width: 1024px)" srcSet={desktopNightArcade} />
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-contain"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-contain"
 							/>
 						</picture>
 					</div>
 
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] top-[3.25rem] w-[6.75rem] text-white">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] w-[6.75rem] text-white">
 							Soccer Team VR
 						</h3>
 						<picture>
 							<source media="(max-width: 480px)" srcSet={mobileSoccerTeam} />
+							<source media="(min-width: 1024px)" srcSet={desktopSoccerTeam} />
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
 
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] top-[3.25rem] w-[3.625rem] text-white ">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] w-[3.625rem] text-white ">
 							The Grid
 						</h3>
 						<picture>
 							<source media="(max-width: 480px)" srcSet={mobileGridImage} />
+							<source media="(min-width: 1024px)" srcSet={desktopGridImage} />
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
 
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] top-[3.25rem] w-[7.6875rem] text-white ">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] w-[7.6875rem] md:w-[10.25rem] text-white ">
 							From Up Above VR
 						</h3>
 						<picture>
 							<source media="(max-width: 480px)" srcSet={mobileFromAbove} />
+							<source media="(min-width: 1024px)" srcSet={desktopFromAbove} />
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] top-[3.25rem] w-[7.375rem] text-white">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] w-[7.375rem] text-white">
 							Pocket Borealis
 						</h3>
 						<picture>
@@ -112,16 +138,20 @@ const Main = () => {
 								media="(max-width: 480px)"
 								srcSet={mobilePocketBorealis}
 							/>
+							<source
+								media="(min-width: 1024px)"
+								srcSet={desktopPocketBorealis}
+							/>
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
 
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] top-[3.25rem] w-[7.9375rem] text-white">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] w-[7.9375rem] text-white">
 							The Curiosity
 						</h3>
 						<picture>
@@ -129,24 +159,29 @@ const Main = () => {
 								media="(max-width: 480px)"
 								srcSet={mobileCuriosityImage}
 							/>
+							<source
+								media="(min-width: 1024px)"
+								srcSet={desktopCuriosityImage}
+							/>
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
 
 					<div className="relative ">
-						<h3 className="absolute left-[1.25rem] top-[3.25rem] w-[6.1875rem] text-white">
+						<h3 className="absolute left-[1.25rem] top-[3.25rem] md:left-[2.5rem] md:top-[22.12rem] w-[6.1875rem] md:w-[8.25rem] text-white">
 							Make it Fisheye
 						</h3>
 						<picture>
 							<source media="(max-width: 480px)" srcSet={mobileFisheye} />
+							<source media="(min-width: 1024px)" srcSet={desktopFisheye} />
 							<img
 								src={mobileDeepEarth}
 								alt=""
-								className="w-full h-[7.5rem] object-cover"
+								className="w-full h-[7.5rem] md:w-[16rem] md:h-[28.125rem] object-cover"
 							/>
 						</picture>
 					</div>
@@ -154,7 +189,9 @@ const Main = () => {
 			</section>
 			<div className="flex justify-center">
 				<button className="mt-[2.13rem] border border-black py-[.81rem] pl-[2.38rem] pr-[2.69rem]">
-					<h4 className="uppercase text-[.875rem] tracking-[.3125rem] font-[400] font-alata">See All</h4>
+					<h4 className="uppercase text-[.875rem] tracking-[.3125rem] font-[400] font-alata">
+						See All
+					</h4>
 				</button>
 			</div>
 		</main>
